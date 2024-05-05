9650074f0ae225cabca629f98aa2a65022899056
TheOrbMaverick <enangita@gmail.com>
2024-05-05 15:23:59 +0100

    understanding

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    const checkedAmenities = {};
+  
+    function updateAmenitiesList() {
+      const amenitiesList = Object.values(checkedAmenities).join(", ");
+      $(".amenities > h4").text(amenitiesList);
+    }
+  
+    $(".amenities input[type='checkbox']").change(function () {
+      const amenityId = $(this).closest("li").data("id");
+  
+      if ($(this).is(":checked")) {
+        checkedAmenities[amenityId] = $(this).closest("li").data("name");
+      } else {
+        delete checkedAmenities[amenityId];
+      }
+  
+      updateAmenitiesList();
+    });
+  
+    const url = 'http://0.0.0.0:5001/api/v1/status/';
+  
+    function updateApiStatus() {
+      $.ajax({
+        type: 'GET',
+        url: url,
+        success: function (data) {
+          if (data.status === 'OK') {
+            $('#api_status').addClass('available');
+          } else {
+            $('#api_status').removeClass('available');
+          }
+        }
+      });
+    }
+  
+    updateApiStatus(); // Initial update
+});
